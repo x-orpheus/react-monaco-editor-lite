06cd4c9118ab12c8cfc6363fc889ddbb61a3aca2
wwsun <ww.sww@outlook.com>
2023-06-20 17:52:15 +0800

    feat: disable internal prettier

--- src/utils/initEditor.ts
+++ src/utils/initEditor.ts
@@ -182,28 +182,28 @@ export const startUp = () => {
 
       // eslint-disable-next-line @typescript-eslint/no-empty-function
       window.require(['vs/editor/editor.main'], () => {});
-      window.define(
-        'prettier',
-        [
-          'https://unpkg.com/prettier@2.5.1/standalone.js',
-          'https://unpkg.com/prettier@2.5.1/parser-babel.js',
-          'https://unpkg.com/prettier@2.5.1/parser-html.js',
-          'https://unpkg.com/prettier@2.5.1/parser-postcss.js',
-          'https://unpkg.com/prettier@2.5.1/parser-typescript.js',
-        ],
-        (prettier: any, ...args: any[]) => {
-          const prettierPlugins = {
-            babel: args[0],
-            html: args[1],
-            postcss: args[2],
-            typescript: args[3],
-          };
-          return {
-            prettier,
-            prettierPlugins,
-          };
-        }
-      );
+      // window.define(
+      //   'prettier',
+      //   [
+      //     'https://unpkg.com/prettier@2.5.1/standalone.js',
+      //     'https://unpkg.com/prettier@2.5.1/parser-babel.js',
+      //     'https://unpkg.com/prettier@2.5.1/parser-html.js',
+      //     'https://unpkg.com/prettier@2.5.1/parser-postcss.js',
+      //     'https://unpkg.com/prettier@2.5.1/parser-typescript.js',
+      //   ],
+      //   (prettier: any, ...args: any[]) => {
+      //     const prettierPlugins = {
+      //       babel: args[0],
+      //       html: args[1],
+      //       postcss: args[2],
+      //       typescript: args[3],
+      //     };
+      //     return {
+      //       prettier,
+      //       prettierPlugins,
+      //     };
+      //   }
+      // );
     }
   );
   const interval = setInterval(() => {
